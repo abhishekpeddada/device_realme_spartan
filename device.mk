d451b5663447a95ab4c4edacbe612fe4844e912e
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2023-07-20 07:56:17 +0530

    spartan: camera : add support for oplus camera

--- device.mk
+++ device.mk
@@ -137,6 +137,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Camera
+$(call inherit-product, vendor/oplus/camera/opluscamera.mk)
+
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
