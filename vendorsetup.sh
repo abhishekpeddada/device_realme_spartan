f801e8d1bed46ce6a7d22412171046f2bdcdb1ca
“OnFire-Nasir” <nasironfire07@gmail.com>
2023-07-30 10:41:52 +0000

    spartan: add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/RealmeGTNeo3T-Devs/proprietary_vendor_realme_spartan vendor/realme/spartan
+git clone https://github.com/RealmeGTNeo3T-Devs/android_kernel_realme_sm8250 kernel/realme/sm8250
+git clone https://github.com/abhishekpeddada/android_hardware_oplus hardware/oplus
+git clone https://gitlab.com/pjgowtham/proprietary_vendor_oplus_camera/ vendor/oplus/camera
